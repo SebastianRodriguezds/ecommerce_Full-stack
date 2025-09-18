503a6c367ea498cc9190ca5c84a87f298637cfc7
Sebastian Rodriguez <sebastianrodriguezds@gmail.com>
2025-09-18 18:46:14 -0300

    feat: CategoryPage with dynamic routing

--- frontend/src/pages/home/Categories.jsx
+++ frontend/src/pages/home/Categories.jsx
@@ -10,7 +10,7 @@ export const Categories = () => {
   const categories = [
     {name: 'Accessories', path:'accessories', image: category1 },
     {name: 'Dress Collection', path:'dress', image: category2 },
-    {name: 'Jewellery', path:'Jewellery', image: category3 },
+    {name: 'Jewellery', path:'jewellery', image: category3 },
     {name: 'Cosmetics', path:'cosmetics', image: category4 },
   ]
   return (
