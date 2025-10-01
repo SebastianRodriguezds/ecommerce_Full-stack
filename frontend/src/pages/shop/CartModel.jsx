cf781ea0d811a312bf581006cbffa906632b96a5
Sebastian Rodriguez <sebastianrodriguezds@gmail.com>
2025-10-01 13:55:53 -0300

    feat(cart): add quantity update(+/-) and clear cart functionality

--- frontend/src/pages/shop/CartModel.jsx
+++ frontend/src/pages/shop/CartModel.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import OrderSummary from './OrderSummary'
+import { useDispatch } from 'react-redux'
+import { updateQuantity, removeFromCart, clearCart } from '../../redux/features/cart/cartSlice'
 
 const CartModel = ({ products, isOpen, onClose }) => {
+    const dispatch = useDispatch();
+
+    const handleQuantity = (type, id) => {
+        const payload = {type, id}
+        dispatch(updateQuantity(payload))
+    }
+
+    const handleRemove = (e, id) => {
+        e.preventDefault()
+        dispatch(removeFromCart({id}))
+    }
     return (
         <div className={`fixed z-[1000] inset-0 bg-black bg-opacity-80 transition-opacity ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
             style={{ transition: 'opacity 300ms' }}
@@ -32,13 +45,13 @@ const CartModel = ({ products, isOpen, onClose }) => {
                                             </div>
 
                                             <div className='flex flex-row md:justify-start justify-end items-center mt-2'>
-                                                <button className='size-6 flex items-center justify-center px-1.5 rounded-full bg-gray-200 text-gray-700 hover:bg-primary hover:text-white ml-8'>
+                                                <button onClick={()=> handleQuantity('decrement', item.id)} className='size-6 flex items-center justify-center px-1.5 rounded-full bg-gray-200 text-gray-700 hover:bg-primary hover:text-white ml-8'>
                                                     -
                                                 </button>
                                                 <span className='px-2 text-center mx-1'>{item.quantity}</span>
-                                                <button className='size-6 flex items-center justify-center px-1.5 rounded-full bg-gray-200 text-gray-700 hover:bg-primary hover:text-white'>+</button>
+                                                <button onClick={()=> handleQuantity('increment', item.id)} className='size-6 flex items-center justify-center px-1.5 rounded-full bg-gray-200 text-gray-700 hover:bg-primary hover:text-white'>+</button>
                                                 <div className='ml-5'>
-                                                    <button className='text-red-500 hover:text-red-800 mr-4'>Remove</button>
+                                                    <button onClick={(e)=> handleRemove(e, item.id)} className='text-red-500 hover:text-red-800 mr-4'>Remove</button>
                                                 </div>
 
                                             </div>
--- frontend/src/pages/shop/OrderSummary.jsx
+++ frontend/src/pages/shop/OrderSummary.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import {useSelector} from "react-redux"
+import {useDispatch, useSelector} from "react-redux"
+import { clearCart } from '../../redux/features/cart/cartSlice';
 
 const OrderSummary = () => {
+  const distpach = useDispatch()
   const products = useSelector((store) => store.cart.products);
   const {selectedItems, totalPrice, tax, taxRate, grandTotal} = useSelector((store)=>store.cart);
+
+  const handleClearCart = () => {
+    distpach(clearCart())
+  }
+
   return (
     <div className='bg-primary-light mt-5 rounded text-base'>
     <div className='px-6 py-4 space-y-5'>
@@ -13,7 +20,10 @@ const OrderSummary = () => {
         <p>Tax: ({taxRate * 100}%): ${tax.toFixed(2)}</p>
         <h3 className='font-bold'>Grand Total: ${grandTotal.toFixed(2)}</h3>
         <div className='px-4 mb-6'>
-            <button className='bg-red-500 px-3 py-1.5 text-white mt-2 rounded-md flex justify-between items-center mb-4'><span className='mr-2'>Clear cart</span><i className='ri-delete-bin-7-fill'></i></button>
+            <button onClick={(e)=> {
+                e.stopPropagation();
+                handleClearCart();
+            }} className='bg-red-500 px-3 py-1.5 text-white mt-2 rounded-md flex justify-between items-center mb-4'><span className='mr-2'>Clear cart</span><i className='ri-delete-bin-7-fill'></i></button>
             <button className='bg-green-600 px-3 py-1.5 text-white mt-2 rounded-md flex justify-between items-center'><span className='mr-2'>Procced Checkout</span> <i className='ri-bank-card-line'></i></button>
         </div>
     </div>
