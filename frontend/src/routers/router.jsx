857699eb14873cab5af1aa7b595e8511d4cc4f91
Sebastian Rodriguez <sebastianrodriguezds@gmail.com>
2025-10-03 20:48:53 -0300

    Login and Register components UI added

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -5,6 +5,8 @@ import CategoryPage from "../pages/category/CategoryPage";
 import Search from "../pages/search/Search"
 import ShopPage from "../pages/shop/ShopPage"
 import SingleProduct from "../pages/shop/productDetails/SingleProduct"
+import Login from "../components/Login";
+import Register from "../components/Register";
 
 const router = createBrowserRouter([
     {
@@ -18,6 +20,14 @@ const router = createBrowserRouter([
             { path: "/shop/:id", element: <SingleProduct /> }
         ]
     },
+    {
+        path: "/login",
+        element: <Login/>
+    },
+    {
+        path: "/register",
+        element: <Register />
+    }
 ]);
 
 export default router;
