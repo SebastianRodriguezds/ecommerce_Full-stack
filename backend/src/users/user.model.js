a22d88d58ad736d4e15f375f9e8fed313530febb
Sebastian Rodriguez <sebastianrodriguezds@gmail.com>
2025-10-12 22:02:32 +0100

    feat: login endpoint and bcrypt for password hashing added

--- backend/src/users/user.model.js
+++ backend/src/users/user.model.js
@@ -1,17 +1,32 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 const userSchema = new Schema({
-    username: {type: String, require: true, unique: true},
-    email: {type: String, require: true, unique: true},
-    password: {type: String, require: true},
-    rola: {type: String, default: 'user'},
-    profileImage: {String, 
-        bio: {type: String, maxlength: 200}},
-        profession: String,
-        createdAt: {
-            type: Date,
-            default: Date.now
-        }
-})
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: 'user' },
+    profileImage: String,
+    bio: { type: String, maxlength: 200 },
+    profession: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// hashing password
+userSchema.pre('save', async function(next){
+    const user = this;
+    if(!user.isModified('password')) return next();
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next();
+});
+
+// match passwords
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password)
+};
 
 const User = new model('User', userSchema);
 module.exports = User;
--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -3,15 +3,37 @@ const User = require('./user.model');
 const router = express.Router();
 
 //Register endpoint
-router.post('/register', async (req, res)=> {
-    try{
-        const {username, email, password} = req.body;
-        const user = new User({email, username, password});
+router.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = new User({ email, username, password });
         await user.save();
-        res.status(201).send({message: "User registered successfullly.."})
-    }catch (error){
-
+        res.status(201).send({ message: "User registered successfully.." })
+    } catch (error) {
+        console.log("Error registering user", error);
+        res.status(500).send({ error: "Error registering user." });
     }
-})
+});
+
+// login user endpoint
+router.post("/login", async (req, res)=> {
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+    if(!user) {
+        return res.status(404).send({message: 'User not found'})
+    }
+    const isMatch = await user.comparePassword(password);
+    if(!isMatch) {
+        return res.status(404).send({message: 'Password not match'});
+    }
+    res.status(200).send({message: 'logged in successfully', user});
+    }catch (err) {
+        console.log('Error logged in user', err);
+        res.status(401).send({message: 'Error logged in user'})
+    }
+});
+
+
 
 module.exports = router;
